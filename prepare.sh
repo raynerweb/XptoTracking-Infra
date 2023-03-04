0357d639a43e7f842eac1d15ce13675e5c689a65
raynerweb <raynerweb@gmail.com>
2023-03-04 01:48:06 +0000

    downloading all project

--- prepare.sh
+++ prepare.sh
@@ -18,9 +18,24 @@ then
     exit 99
 fi
 
+wget -c https://github.com/raynerweb/telemetry/archive/refs/heads/master.zip
+mv master.zip telemetry-master.zip
+unzip telemetry-master.zip
+docker run -it -v "$HOME/.m2":/root/.m2 -v "$(pwd)/telemetry-master":/usr/src/mymaven -w /usr/src/mymaven maven:3.9.0 mvn -Pdev clean install
+docker build -t telemetry telemetry-master/
+
 wget -c https://github.com/raynerweb/tracking/archive/refs/heads/master.zip
 mv master.zip tracking-master.zip
 unzip tracking-master.zip
 docker run -it -v "$HOME/.m2":/root/.m2 -v "$(pwd)/tracking-master":/usr/src/mymaven -w /usr/src/mymaven maven:3.9.0 mvn -Pdev clean install
 docker build -t tracking tracking-master/
 
+wget -c https://github.com/raynerweb/vehicle-python/archive/refs/heads/master.zip
+mv master.zip vehicle-python-master.zip
+unzip vehicle-python-master.zip
+docker build -t vehicle vehicle-python-master/VehiclesMS/
+
+wget -c https://github.com/raynerweb/people/archive/refs/heads/master.zip
+mv master.zip people-master.zip
+unzip people-master.zip
+docker build -t people people-master/
